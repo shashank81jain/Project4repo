06975231d6f85afe079d281ad4b20ce5c929629f
shashank81jain <79475263+shashank81jain@users.noreply.github.com>
2022-01-16 13:26:11 +0530

    Update run_kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,11 +4,11 @@
 
 # Step 1:
 # This is your Docker ID/path
-dockerpath=namit11/devopsproj
+dockerpath=shashak81jain/Project4repo
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run devopsproj --image=$dockerpath --port=8080
+kubectl run Project4repo --image=$dockerpath --port=8080
 
 # Step 3:
 # List kubernetes pods
@@ -16,6 +16,6 @@ kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-kubectl port-forward pods/devopsproj 8080:80
+kubectl port-forward pods/Project4repo8080:80
 
 cmd /k
